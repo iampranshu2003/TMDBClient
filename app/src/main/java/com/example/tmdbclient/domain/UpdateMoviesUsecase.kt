77c11cb79c744958b4202c693fe8e5a69c3f4ccd
Pranshu Gupta <iampranshu2003@gmail.com>
2025-02-15 23:51:13 +0530

    domain layer work in progress added update and get movies

--- app/src/main/java/com/example/tmdbclient/domain/UpdateMoviesUsecase.kt
+++ app/src/main/java/com/example/tmdbclient/domain/UpdateMoviesUsecase.kt
@@ -1,4 +1,8 @@
 package com.example.tmdbclient.domain
 
+import com.example.tmdbclient.data.data.model.movie.Movie
+
 class UpdateMoviesUsecase(private val movieRepository: MovieRepository) {
+    suspend fun updateMovies():List<Movie>? = movieRepository.updateMovies()
+    
 }
