a39021e66c40d3964ca13ea66d38df01af710b21
Pranshu Gupta <iampranshu2003@gmail.com>
2025-02-23 18:27:10 +0530

    completed domain layer

--- app/src/main/java/com/example/tmdbclient/domain/usecase/UpdateMoviesUsecase.kt
+++ app/src/main/java/com/example/tmdbclient/domain/usecase/UpdateMoviesUsecase.kt
@@ -1,6 +1,7 @@
-package com.example.tmdbclient.domain
+package com.example.tmdbclient.domain.usecase
 
 import com.example.tmdbclient.data.data.model.movie.Movie
+import com.example.tmdbclient.domain.repository.MovieRepository
 
 class UpdateMoviesUsecase(private val movieRepository: MovieRepository) {
     suspend fun updateMovies():List<Movie>? = movieRepository.updateMovies()
